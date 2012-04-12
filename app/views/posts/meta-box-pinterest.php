a6f6fb3173ab13d7d48b54e24cc73f307c4e1de6
Blair Williams <blair@caseproof.com>
2012-04-12 18:30:48 +0000

    Fixed a conflict between the wordpress post editor and the pinterest featured image selector

--- app/views/posts/meta-box-pinterest.php
+++ app/views/posts/meta-box-pinterest.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');} ?>
 <label for="<?php echo $soma_post_options->pinterest_image_str; ?>">
-  <input type="text" name="<?php echo $soma_post_options->pinterest_image_str; ?>" name="<?php echo $soma_post_options->pinterest_image_str; ?>" value="<?php echo esc_url( $soma_post_options->pinterest_image ); ?>" />
+  <input type="text" id="<?php echo $soma_post_options->pinterest_image_str; ?>" name="<?php echo $soma_post_options->pinterest_image_str; ?>" value="<?php echo esc_url( $soma_post_options->pinterest_image ); ?>" />
   <input type="button" id="pinterest-image-upload" class="soma-upload-button button-primary" value="<?php _e('Choose...', 'social-maven');?>" />
   <span class="description"><?php _e('Select an image that will be used on Pinterest when pinning this post.', 'social-maven'); ?></span>
 </label>
--- js/admin-posts.js
+++ js/admin-posts.js
@@ -1,17 +1,31 @@
+var uploadID          = '';
+var storeSendToEditor = '';
+var newSendToEditor   = '';
+
 (function ($) {
   $(document).ready(function(e) {
     var formfield;
     
     // Image uploader
     $('.soma-upload-button').click(function() {
-      formfield = $(this).prev();
+      uploadID = $(this).prev().attr('id');
+      window.send_to_editor = newSendToEditor;
       tb_show('', 'media-upload.php?type=image&amp;TB_iframe=1');
     });
+
+    storeSendToEditor = window.send_to_editor;
+    newSendToEditor   = function(html) { imgurl = $('img',html).attr('src');
+                                         $("#" + uploadID).val(imgurl);
+                                         tb_remove();
+                                         window.send_to_editor = storeSendToEditor;
+                                       };
      
+    /*
     window.send_to_editor = function(html) {
       imgurl = $('img',html).attr('src');
       formfield.val(imgurl);
       tb_remove();
     }
+    */
   });
 })(jQuery);
